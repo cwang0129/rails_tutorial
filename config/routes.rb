09a9e988531f4a600272122f3257d7ad01ba3976
cwang0129 <cwang0129@gmail.com>
2018-03-04 20:39:29 -0800

    initialize repos

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+Rails.application.routes.draw do
+  #root 'application#hello'
+  root 'application#goodbye'
+end
+
